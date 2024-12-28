4b13080ffeb0e2772cdc0fd0a93e02237c0d035c
AndreaR97 <lillylini610@gmail.com>
2024-12-28 18:53:22 +0100

    Revert "offerride formatierung"
    This reverts commit 6dab07d1106f6bee65db2979ff4d2934aef3f132.

--- src/components/BookRide.vue
+++ src/components/BookRide.vue
@@ -1,4 +1,5 @@
 <template>
+  <div class="bookride">
     <NavigationBar></NavigationBar>
 
     <!--<img src="/assets/profilpage.png" alt="ProfilePageBackground" class="profilebackground" />-->
@@ -15,6 +16,7 @@
             @click="setStep(0)"
           ></v-stepper-item>
 
+          <v-divider></v-divider>
 
           <v-stepper-item
             title="Wähle eine Fahrt aus"
@@ -314,3 +316,353 @@
     </v-fade-transition>
   </div>
 </template>
+
+
+<script setup>
+import { ref } from 'vue'
+
+const dialog = ref(false) //dient dem Öffnen und Schließen des Popup-Dialogs
+const overlay = ref(false)  //dient dem Overlay für den Popup-Dialog
+</script>
+
+<script>
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+import MapComponent from './MapComponent.vue';
+import NavigationBar from './NavigationBar.vue';
+
+export default {
+ 
+  data() {
+    return {
+      drawer: false,
+      group: null,
+      step: 0,
+      selected: 0,
+
+      items: [
+        { title: 'Foo', value: 'foo' },
+        { title: 'Bar', value: 'bar' },
+      ],
+
+      rideOffers: [
+      {
+        name: 'Cara',
+        role: 'Student',
+        time: '10:30 Uhr',
+        price: "5,00€",
+        seats: "4",
+        rideid: "4"
+      },
+      {
+        name: 'Johannes',
+        role: 'Student',
+        time: '11:40 Uhr',
+        price: "5,00€",
+        seats: "4",
+        rideid: "5"
+      },
+        {
+        name: 'Maria',
+        role: 'Student',
+        time: '11:50 Uhr',
+        price: "7,50€",
+        seats: "4",
+        rideid: "500"
+      },
+      {
+        name: 'Sarah',
+        role: 'Mitarbeiter',
+        time: '11:50 Uhr',
+        price: "2,50€",
+        seats: "1",
+        rideid: "1"
+      },
+      {
+        name: 'Manuel',
+        role: 'Mitarbeiter',
+        time: '14:00 Uhr',
+        price: "6,50€",
+        seats: "4",
+        rideid: "7"
+      },
+      {
+        name: 'Noah',
+        role: 'Student',
+        time: '11:50 Uhr',
+        price: "2,90€",
+        seats: "3",
+        rideid: "2"
+      },
+      {
+        name: 'Manuel',
+        role: 'Student',
+        time: '12:10 Uhr',
+        price: "6,50€",
+        seats: "4",
+        rideid: "9"
+      },
+      ],
+
+      ride: [
+      {
+        name: 'Allison',
+        surname: 'Peters',
+        role: 'Student',
+        rideid: "4"
+      },
+      {
+        name: 'Thomas',
+        surname: 'Meyer',
+        role: 'Student',
+        rideid: "4"
+      },
+      {
+        name: 'Maria',
+        surname: 'Bauer',
+        role: 'Student',
+        rideid: "4"
+      }
+      ],
+
+      map: null,
+      startLocation: '',
+      endLocation: '',
+      date: '',
+      time: '',
+      freeSeats: '',
+      dateRule: value => {
+        const today = new Date();
+        const maxDate = new Date();
+        maxDate.setDate(today.getDate() + 28); // das datum muss in den näschten 4 wochen liegen
+        const selectedDate = new Date(value);
+        return selectedDate >= today && selectedDate <= maxDate || 'Datum muss innerhalb der nächsten 4 Wochen liegen';
+      },
+      freeSeatsRule: value => {
+        const number = parseInt(value, 10);
+        return number >= 1 && number <= 9 || 'Freie Plätze müssen eine ganze Zahl zwischen 1 und 9 sein';
+      },
+    };
+  },
+  
+
+  methods: {
+    setStep(a) { this.step = a; },
+    initMap() {
+      const uniBayreuthCoords = [49.928809, 11.585835];
+      this.map = L.map("map").setView(uniBayreuthCoords, 15);
+
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: "© OpenStreetMap contributors",
+      }).addTo(this.map);
+
+      const defaultIcon = L.icon({
+        iconUrl: markerIcon,
+        shadowUrl: markerShadow,
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41],
+      });
+      L.Marker.prototype.options.icon = defaultIcon;
+
+      L.marker(uniBayreuthCoords).addTo(this.map);
+    },
+    swapLocations() {
+      const temp = this.startLocation;
+      this.startLocation = this.endLocation;
+      this.endLocation = temp;
+    },
+  },
+  mounted() {
+    this.initMap();
+  }
+};
+</script>
+
+<style scoped>
+
+
+
+
+.v-sheet.v-theme-light--v-stepper {
+  height: 100vh !important;       /* Mit !important erzwingt man die Anwendung der Regel */
+  min-height: 100vh !important;
+  width: 100vw !important;
+  display: flex !important;
+  flex-direction: column !important;
+  justify-content: flex-start !important;
+  align-items: center !important;
+  padding: 20px;
+  box-sizing: border-box;
+}
+
+
+
+.bookride {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  z-index: 1; /* Ensure it does not cover other components */
+}
+
+.row {
+  display: flex;
+  width: 100%;
+  height: 25%; /* Jede Row nimmt 25% der Containerhöhe ein */
+}
+
+.col {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+}
+
+.profilebackground {
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  object-fit: cover;
+  position: absolute;
+}
+
+.map-container {
+  display: flex;
+  justify-content: flex-end;
+  align-items: start;
+  height: calc(100vh - 64px); /* damit man nicht scrollen kann */
+  padding-right: 100px;
+}
+.independent-map {
+  width: 600px;
+  height: 600px;
+  border-radius: 15px;
+  z-index: 500;
+}
+
+.v-text-field input {
+  font-size: 1.4em;
+  font-weight: 100;
+  text-transform: capitalize;
+}
+
+.v-text-field label {
+  font-size: 1.1em;
+  font-weight: 100;
+}
+
+.Startlocation {
+  position: absolute;
+  top: 240px;
+  left: 100px;
+  width: 700px;
+  height: 150px;
+  z-index: 1000;
+  font-family: 'Minion Pro Italic';
+  font-size: 30px;
+}
+
+.Endlocation {
+  position: absolute;
+  top: 390px;
+  left: 100px;
+  width: 700px;
+  height: 150px;
+  z-index: 1000;
+  font-family: 'Minion Pro Italic';
+  font-size: 30px;
+}
+
+.Date {
+  position: absolute;
+  top: 540px;
+  left: 100px;
+  width: 275px;
+  height: 150px;
+  z-index: 1000;
+  font-family: 'Minion Pro Italic';
+  font-size: 30px;
+}
+
+.Time {
+  position: absolute;
+  top: 540px;
+  left: 525px;
+  width: 275px;
+  height: 150px;
+  z-index: 1000;
+  font-family: 'Minion Pro Italic';
+  font-size: 30px;
+}
+
+.Freeseats {
+  position: absolute;
+  top: 690px;
+  left: 100px;
+  width: 250px;
+  height: 150px;
+  z-index: 1000;
+  font-family: 'Minion Pro Italic';
+  font-size: 30px;
+}
+
+.Submitbutton {
+  position: absolute;
+  top: 690px;
+  left: 500px;
+  width: 300px;
+  height: 100px;
+  z-index: 1000;
+  font-family: 'Minion Pro Italic';
+  font-size: 20px;
+  color: #48535A;
+}
+
+.SwapButton {
+  position: absolute;
+  top: 320px;
+  left: 650px;
+  z-index: 1000;
+  width: 80px;
+  height: 80px;
+}
+
+.icon-container {
+  animation: fadeIn 0.4s;
+}
+
+/*Icon animation */
+@keyframes fadeIn {
+  0% {
+    transform: translateX(0) scale(0);
+    opacity: 0;
+  }
+  45% {
+    transform: translate(0) scale(1.3);
+    opacity: 1;
+  }
+  75% {
+    transform: translate(0) scale(.9);
+  }
+  100% {
+    transform: translateX(0) scale(1, 1);
+    opacity: 1;
+  }
+}
+
+.thick-black-divider {
+  border-left: 40px solid black;
+}
+
+.left-column {
+  padding: 50px;
+}
+
+.right-column {
+  padding: 50px;
+}
+</style>
